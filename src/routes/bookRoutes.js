8dea717b7ac93add4d3094730cd97a8b21ad0d28
Rafi <quazirafi1@gmail.com>
2020-04-28 22:19:06 +0600

    controller and 3rd party api

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,75 +1,15 @@
 const express = require('express');
-
-const { MongoClient, ObjectID } = require('mongodb');
-
-const debug = require('debug')('app:bookRoutes');
+const bookController = require('../controllers/bookController');
 
 const bookRouter = express.Router();
+const bookService = require('../services/goodreadsService');
 
 function router(nav) {
-	bookRouter.use((req, res, next) => {
-		if (req.user) {
-			next();
-		} else {
-			res.redirect('/');
-		}
-	});
-	bookRouter.route('/').get((req, res) => {
-		const url = 'mongodb://localhost:27017';
-		const dbName = 'libraryApp';
+	const { getIndex, getById, middleware } = bookController(bookService, nav);
+	bookRouter.use(middleware);
+	bookRouter.route('/').get(getIndex);
 
-		(async function mongo() {
-			let client;
-			try {
-				client = await MongoClient.connect(url);
-				debug('Connected correctly');
-
-				const db = client.db(dbName);
-
-				const col = await db.collection('books');
-
-				const books = await col.find().toArray();
-
-				res.render('bookListView', {
-					nav,
-					title: 'Library',
-					books
-				});
-			} catch (err) {
-				debug(err.stack);
-			}
-			client.close();
-		})();
-	});
-
-	bookRouter.route('/:id').get((req, res) => {
-		const { id } = req.params;
-		const url = 'mongodb://localhost:27017';
-		const dbName = 'libraryApp';
-		(async function mongo() {
-			let client;
-			try {
-				client = await MongoClient.connect(url);
-				debug('Connected correctly');
-
-				const db = client.db(dbName);
-
-				const col = await db.collection('books');
-
-				const book = await col.findOne({ _id: ObjectID(id) });
-
-				debug(book);
-
-				res.render('bookView', {
-					nav,
-					title: 'Library',
-					book
-				});
-			} catch (err) {
-				debug(err.stack);
-			}
-		})();
-	});
+	bookRouter.route('/:id').get(getById);
 	return bookRouter;
 }
 
